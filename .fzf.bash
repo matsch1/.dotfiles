23dc74a1adc9c1416e4ebdcc082b3b05f1fb4432
Matthias Schaefer <matsch1@gmx.net>
2024-11-16 19:15:10 +0100

    first commit using stow as a symbolic link manager

--- .fzf.bash
+++
@@ -1,9 +0,0 @@
-export FZF_CTRL_T_OPTS="
-  --walker-skip .git,node_modules,target
-  --preview 'bat -n --color=always {}'
-  --bind 'ctrl-/:change-preview-window(down|hidden|)'"
-
-export FZF_CTRL_R_OPTS="
-  --bind 'ctrl-y:execute-silent(echo -n {2..} | pbcopy)+abort'
-  --color header:italic
-  --header 'Press CTRL-Y to copy command into clipboard'"
