73262e65573995ff9f117d5e3fcc95d8917afdc8
Matthias Schaefer <matthias.schaefer@itk-engineering.de>
2024-11-20 16:29:28 +0100

    enable navigation with neo-tree

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,31 +1,43 @@
--- bootstrap lazy.nvim, LazyVim and your plugins
+-- Bootstrap lazy.nvim, LazyVim, and your plugins
 require("config.lazy")
 
+-- General settings
 vim.opt.clipboard = "unnamedplus"
+vim.opt.expandtab = true
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.swapfile = false
+vim.wo.number = true
+vim.g.mapleader = " "
+vim.g.background = "light"
 
+-- Neo-tree setup
 require("neo-tree").setup({
-
   filesystem = {
-
     filtered_items = {
-
       hide_dotfiles = false, -- Show hidden files (dotfiles)
     },
   },
 })
-vim.cmd("set expandtab")
-vim.cmd("set tabstop=2")
-vim.cmd("set softtabstop=2")
-vim.cmd("set shiftwidth=2")
-vim.g.mapleader = " "
-vim.g.background = "light"
 
-vim.opt.swapfile = false
+-- Keybindings
 
-vim.keymap.set("n", "<c-k>", ":wincmd k<CR>")
-vim.keymap.set("n", "<c-j>", ":wincmd j<CR>")
-vim.keymap.set("n", "<c-h>", ":wincmd h<CR>")
-vim.keymap.set("n", "<c-l>", ":wincmd l<CR>")
+-- Window navigation
+vim.keymap.set("n", "<C-k>", ":wincmd k<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<C-j>", ":wincmd j<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<C-h>", ":wincmd h<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<C-l>", ":wincmd l<CR>", { noremap = true, silent = true })
 
-vim.keymap.set("n", "<leader>h", ":nohlsearch<CR>")
-vim.wo.number = true
+-- Neo-tree keybindings
+vim.keymap.set("n", "<C-e>", ":Neotree toggle<CR>", { noremap = true, silent = true }) -- Toggle Neo-tree
+
+-- Toggle focus between Neo-tree and editor using Ctrl+w
+vim.keymap.set("n", "<C-w>", function()
+  -- Check if the current window is Neo-tree
+  if vim.bo.filetype == "neo-tree" then
+    vim.cmd("wincmd l") -- Move focus to the next window
+  else
+    vim.cmd("Neotree focus") -- Focus Neo-tree
+  end
+end, { noremap = true, silent = true })
