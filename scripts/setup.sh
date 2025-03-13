7eed6e5a13448d4088f56d6d774434822718f3fe
Matthias Schaefer <matthias.schaefer@itk-engineering.de>
2025-03-13 07:56:46 +0100

    add setup script

---
+++ scripts/setup.sh
@@ -0,0 +1,80 @@
+#!/bin/bash
+
+# This script installs specified packages on a Debian- or Arch-based Linux system.
+# It detects the user's operating system, determines the appropriate package manager,
+# and installs the given packages. If no packages are specified as arguments, a default
+# set of packages is installed. The script also logs installation results and
+# stows dotfiles for installed packages.
+
+# Usage:
+#   ./install_packages.sh [USER] [PACKAGE1] [PACKAGE2] ...
+#
+#   - USER (optional): The username for whom the packages will be installed. Defaults to the current user.
+#   - PACKAGE1, PACKAGE2, ... (optional): List of packages to install. If not provided, a default list is used.
+#
+
+# user
+user=${1:-$USER}
+
+# List of packages to install
+if [[ $# -gt 1 ]]; then
+  packages=("${@:2}") # Use arguments starting from position 2
+else
+  packages=("stow" "bash" "git" "tmux" "fzf" "neovim" "conky" "alacritty")
+fi
+
+# Function to install a package
+install_package() {
+  package=$1
+
+  # Check OS
+  os=$(grep '^ID=' /etc/os-release | cut -d= -f2 | tr -d '"')
+  debian_os=("debian" "ubuntu" "pop" "linuxmint" "elementary" "zorin" "kali" "parrot" "raspbian" "devuan")
+  arch_os=("arch" "manjaro" "endeavouros" "arcolinux" "garuda" "artix" "blackarch" "rebornos")
+
+  # Set installation command
+  if [[ " ${debian_os[@]} " =~ " $os " ]]; then
+    echo "Debian based system"
+    installCmd="sudo apt-get install -y"
+  elif [[ " ${debian_os[@]} " =~ " $os " ]]; then
+    echo "Arch based system"
+    installCmd="sudo pacman -S --noconfirm"
+  else
+    echo "Unsupported OS: $os"
+    return 1
+  fi
+
+  # Install the package
+  echo "Installing $package"
+  $installCmd "$package"
+  write_log "$package"
+}
+
+# Function to write installation log
+write_log() {
+  package=$1
+  log_file="$HOME/setup-log.txt"
+
+  if type -p "$package" >/dev/null; then
+    echo "$(date '+%Y-%m-%d %H:%M:%S') - $package Installed" >>"$log_file"
+  else
+    echo "$(date '+%Y-%m-%d %H:%M:%S') - $package FAILED TO INSTALL!!!" >>"$log_file"
+  fi
+}
+
+# ############### INSTALLATION ####################
+read -p "Install for user $user? (y/n): " choice
+if [[ "$choice" != "y" ]]; then
+  echo "Exit by user"
+  exit 1
+fi
+
+for package in "${packages[@]}"; do
+  read -p "Install $package? (y/n): " choice
+  if [[ "$choice" == "y" || "$choice" == "Y" ]]; then
+    install_package "$package"
+    (stow -d .. -t /home/$user $package && echo "$package stowed succesfully") || echo "No dotfiles for $package"
+  else
+    echo "Installation skipped."
+  fi
+done
