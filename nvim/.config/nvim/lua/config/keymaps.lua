373f3ca33384f61c18a4eeb48c0a5aa24afbbb16
Matthias Schaefer <matthias.schaefer@itk-engineering.de>
2025-03-25 15:02:30 +0100

    replace build in explorer with neo-tree

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,6 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+vim.keymap.set("n", "<leader>e", function()
+  require("neo-tree.command").execute({ toggle = true, dir = vim.loop.cwd() })
+end, { desc = "Toggle Neo-tree" })
