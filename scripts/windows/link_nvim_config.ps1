865b4480c659c103ff989762b83efe7cd1490e40
Matthias Schaefer <matthias.schaefer@itk-engineering.de>
2025-03-27 10:03:02 +0100

    fix window nvim link for powershell instead of pwsh

--- scripts/windows/link_nvim_config.ps1
+++ scripts/windows/link_nvim_config.ps1
@@ -1,15 +1,16 @@
 # link NeoVim configuration
 echo "link NeoVim config"
 $dotFilesRoot = Join-Path $HOME ".dotfiles"
-$dotfilesConfiguration = Join-Path $dotFilesRoot "nvim" ".config" "nvim"
+$dotfilesConfiguration = $dotFilesRoot | Join-Path -ChildPath "nvim/.config/nvim"
 $localConfiguration = Join-Path $env:LOCALAPPDATA "nvim"
 
-echo "doftifleConfig: $dotfilesConfiguration"
-echo "localConfig: $localConfiguration"
+echo "dotFilesRoot: $dotFilesRoot"
+echo "dotfilesConfiguration: $dotfilesConfiguration"
+echo "localConfiguration: $localConfiguration"
 
 
 if (!(Test-Path $localConfiguration -PathType Container)) { 
 	echo "linking ..."
-    Start-Process -FilePath "pwsh" -ArgumentList "-c New-Item -Path $localConfiguration -ItemType SymbolicLink -Value $dotfilesConfiguration".Split(" ") -Verb runas
+    Start-Process -FilePath "powershell" -ArgumentList "-c New-Item -Path $localConfiguration -ItemType SymbolicLink -Value $dotfilesConfiguration".Split(" ") -Verb runas
 }
 echo "done"
