376a1512939ffe77593b516f42b2e2e2e5ebbc53
Matthias Schaefer <matsch1@gmx.net>
2025-03-27 13:48:44 +0100

    extract parts of .bashrc which are also used in .zshrc

---
+++ shell/aliases.sh
@@ -0,0 +1,14 @@
+# ====================
+# Aliases
+# ====================
+alias ls='ls --color=auto'
+alias ll='ls -lah'
+alias ..='cd ..'
+alias ...='cd ../..'
+alias ....='cd ../../..'
+alias ~='cd ~'
+alias grep='grep --color=auto'
+alias find='find . -type f'
+alias gitgraph='git log --oneline --graph --decorate --color'
+alias lg='lazygit'
+alias ld='lazydocker'
---
+++ shell/envs.sh
@@ -0,0 +1,16 @@
+# ====================
+# Environment Variables
+# ====================
+
+export XDG_CONFIG_HOME=~/.config
+export EDITOR=nvim
+export DOCKER_BUILDKIT=1
+export COMPOSE_DOCKER_CLI_BUILD=0
+export ANDROID_HOME=/home/matsch/android-sdk
+export CHROME_EXECUTABLE=/usr/bin/chromium
+export JAVA_HOME=/usr/lib/jvm/java-17-openjdk
+export PATH=$PATH:$ANDROID_HOME/platform-tools
+export PATH=$PATH:/usr/local/go/bin
+export PATH=$PATH:/usr/bin
+export PATH=$PATH:~/go/bin
+[ -f "~/Files/Documents/.env" ] && source ~/Files/Documents/.env
