62a81417a11d9f01473b95fcf15ca3d3f4f8759c
Matthias Schaefer <matthias.schaefer@itk-engineering.de>
2025-03-28 16:09:32 +0100

    Create a working Go environment including DAB on WSL

--- nvim/.config/nvim/lua/plugins/nvim-dap.lua
+++ nvim/.config/nvim/lua/plugins/nvim-dap.lua
@@ -2,47 +2,50 @@ return {
   "mfussenegger/nvim-dap",
   dependencies = {
     "rcarriga/nvim-dap-ui",
+    "nvim-neotest/nvim-nio",
   },
   config = function()
-    require("dap-ui").setup()
-    require("dap").listeners.before.attach.dapui_config = function()
-      require("dapui").open()
+    local dap = require("dap")
+    local dapui = require("dapui")
+    dapui.setup()
+    dap.listeners.before.attach.dapui_config = function()
+      dapui.open()
     end
-    require("dap").listeners.before.launch.dapui_config = function()
-      require("dapui").open()
+    dap.listeners.before.launch.dapui_config = function()
+      dapui.open()
     end
-    require("dap").listeners.before.event_terminated.dapui_config = function()
-      require("dapui").close()
+    dap.listeners.before.event_terminated.dapui_config = function()
+      dapui.close()
     end
-    require("dap").listeners.before.event_exited.dapui_config = function()
-      require("dapui").close()
+    dap.listeners.before.event_exited.dapui_config = function()
+      dapui.close()
     end
     vim.keymap.set("n", "<F5>", function()
-      require("dap").continue()
+      dap.continue()
     end)
     vim.keymap.set("n", "<F10>", function()
-      require("dap").step_over()
+      dap.step_over()
     end)
     vim.keymap.set("n", "<F11>", function()
-      require("dap").step_into()
+      dap.step_into()
     end)
     vim.keymap.set("n", "<F12>", function()
-      require("dap").step_out()
+      dap.step_out()
     end)
     vim.keymap.set("n", "<Leader>b", function()
-      require("dap").toggle_breakpoint()
+      dap.toggle_breakpoint()
     end)
     vim.keymap.set("n", "<Leader>B", function()
-      require("dap").set_breakpoint()
+      dap.set_breakpoint()
     end)
     vim.keymap.set("n", "<Leader>lp", function()
-      require("dap").set_breakpoint(nil, nil, vim.fn.input("Log point message: "))
+      dap.set_breakpoint(nil, nil, vim.fn.input("Log point message: "))
     end)
     vim.keymap.set("n", "<Leader>dr", function()
-      require("dap").repl.open()
+      dap.repl.open()
     end)
     vim.keymap.set("n", "<Leader>dl", function()
-      require("dap").run_last()
+      dap.run_last()
     end)
     vim.keymap.set({ "n", "v" }, "<Leader>dh", function()
       require("dap.ui.widgets").hover()
@@ -58,5 +61,51 @@ return {
       local widgets = require("dap.ui.widgets")
       widgets.centered_float(widgets.scopes)
     end)
+
+    -- Go DAB
+    dap.adapters.delve = function(callback, config)
+      if config.mode == "remote" and config.request == "attach" then
+        callback({
+          type = "server",
+          host = config.host or "127.0.0.1",
+          port = config.port or "38697",
+        })
+      else
+        callback({
+          type = "server",
+          port = "${port}",
+          executable = {
+            command = "dlv",
+            args = { "dap", "-l", "127.0.0.1:${port}", "--log", "--log-output=dap" },
+            detached = vim.fn.has("win32") == 0,
+          },
+        })
+      end
+    end
+
+    -- https://github.com/go-delve/delve/blob/master/Documentation/usage/dlv_dap.md
+    dap.configurations.go = {
+      {
+        type = "delve",
+        name = "Debug",
+        request = "launch",
+        program = "${file}",
+      },
+      {
+        type = "delve",
+        name = "Debug test", -- configuration for debugging test files
+        request = "launch",
+        mode = "test",
+        program = "${file}",
+      },
+      -- works with go.mod packages and sub packages
+      {
+        type = "delve",
+        name = "Debug test (go.mod)",
+        request = "launch",
+        mode = "test",
+        program = "./${relativeFileDirname}",
+      },
+    }
   end,
 }
