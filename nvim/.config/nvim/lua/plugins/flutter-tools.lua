e3897a261ae8d1dafc7737cae25063f068827131
Matthias Schaefer <matsch1@gmx.net>
2025-03-29 22:29:38 +0100

    bugfix fluttertools

--- nvim/.config/nvim/lua/plugins/flutter-tools.lua
+++ nvim/.config/nvim/lua/plugins/flutter-tools.lua
@@ -1,16 +1,8 @@
 return {
   "akinsho/flutter-tools.nvim",
-  dependencies = { "nvim-lua/plenary.nvim", "stevearc/dressing.nvim" },
-  config = function()
-    require("flutter-tools").setup({
-      dev_log = {
-        -- toggle it when you run without DAP
-        enabled = true,
-        open_cmd = "tabedit",
-      },
-      -- lsp = {
-      --   capabilities = require("cmp_nvim_lsp").default_capabilities(),
-      -- },
-    })
-  end,
+  event = "VeryLazy",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "stevearc/dressing.nvim",
+  },
 }
