7bc842536389ee5ab2f4e1f47baf577a32a02509
Matthias Schaefer <matsch1@gmx.net>
2025-07-29 21:57:19 +0200

    add matschdrive and ssh to backup

--- scripts/matschbackup.sh
+++ scripts/matschbackup.sh
@@ -19,4 +19,4 @@ fi
 
 # Building and running backup
 go build -o backup "$BACKUPREPO/main.go"
-./backup --remote fritznas:/fritz.nas/NAS/Matthias/backups --path /home/"$USER"/src --path /home/"$USER"/.config --path /home/"$USER"/Files --path /home/"$USER"/_Ablage --path /home/"$USER"/obsidian --zip --debug
+./backup --remote fritznas:/fritz.nas/NAS/Matthias/backups --path /home/"$USER"/src --path /home/"$USER"/.config --path /home/"$USER"/Files --path /home/"$USER"/matschdrive --path /home/"$USER"/obsidian --path /home/"$USER"/.ssh --zip --debug
