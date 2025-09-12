1dc84d314e27c6cb8f7083950816d0fa62f69609
Matthias Schaefer <matsch1@gmx.net>
2025-09-12 15:12:07 +0200

    using rlcone copy instead of copy -f for portfolio performance

--- scripts/shutdown.sh
+++ scripts/shutdown.sh
@@ -2,4 +2,4 @@
 
 # portfolio performance
 ## share database with googledrive for phone access
-cp -f /home/matsch/matschdrive/Documents/Finanzen/PortfolioPerformance/Vermoegen.portfolio /home/matsch/googledrive/_Ablage/
+rclone copy /home/matsch/matschdrive/Documents/Finanzen/PortfolioPerformance/Vermoegen.portfolio googledrive:_Ablage/ -v
