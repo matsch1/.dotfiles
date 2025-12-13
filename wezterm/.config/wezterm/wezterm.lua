4224168ca580993e6b39273b4aef5f68f4b22de7
Matthias Schaefer <matsch1@gmx.net>
2025-12-13 09:21:07 +0100

    clean up wezterm config

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -7,12 +7,14 @@ config.color_scheme = "Catppuccin Mocha"
 config.window_background_opacity = 0.7
 config.enable_tab_bar = false
 
+--- Startup Behavior
 wezterm.on("gui-startup", function(cmd)
 	local tab, pane, window = mux.spawn_window(cmd or {})
 	window:gui_window():maximize()
 end)
-
 config.default_prog = { "zellij" }
+
+--- Shutdown Behavior
 config.window_close_confirmation = "NeverPrompt"
 
 return config
