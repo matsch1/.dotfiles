361bae9e1ae2b3ed606633817970a0ad85c9ac38
Matthias Schaefer <matsch1@gmx.net>
2025-12-13 08:58:20 +0100

    add wezterm config

---
+++ wezterm/.config/wezterm/wezterm.lua
@@ -0,0 +1,17 @@
+local wezterm = require("wezterm")
+local mux = wezterm.mux
+local config = {}
+
+--- Styling
+config.color_scheme = "Catppuccin Mocha"
+config.window_background_opacity = 0.7
+config.enable_tab_bar = false
+
+wezterm.on("gui-startup", function(cmd)
+	local tab, pane, window = mux.spawn_window(cmd or {})
+	window:gui_window():maximize()
+end)
+
+config.default_prog = { "zellij" }
+
+return config
