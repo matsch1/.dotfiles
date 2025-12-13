bb71c544a08ee5c237b454c16973a58ef64f9be7
Matthias Schaefer <matsch1@gmx.net>
2025-12-13 09:16:32 +0100

    cleanup projects

--- shell/projects.sh
+++ shell/projects.sh
@@ -2,16 +2,6 @@
 # Projects
 # ====================
 
-############ Shellmaster ###############
-# if [ -d "$HOME/src/shellmaster/" ]; then
-#   alias devshellmaster='~/.dotfiles/scripts/start_tmux.sh ~/src/shellmaster/'
-# fi
-
-############ Golpacker ###############
-# if [ -d "$HOME/src/goalpacer/" ]; then
-#   alias devgoalpacer='~/.dotfiles/scripts/start_tmux.sh ~/src/goalpacer/'
-# fi
-
 ############ Customer stuff (unversioned) ###############
 if [ -d "$HOME/src/cust/" ]; then
   echo "Set up cust"
