df2ae8c9dcdc87f65c055403468d60c2271847d7
Matthias Schaefer <matsch1@gmx.net>
2025-12-14 19:59:22 +0100

    add script for vps backup

---
+++ scripts/vpsbackup.sh
@@ -0,0 +1,3 @@
+rsync -av --mkpath /home/matsch/ matsch@homie:/home/matsch/vpsbak/home/matsch/
+rsync -av --mkpath /data/coolify/ matsch@homie:/home/matsch/vpsbak/data/coolify/
+rsync -av --mkpath /var/lib/docker/ matsch@homie:/home/matsch/vpsbak/var/lib/docker/
