468e4bfc530f9a381154f81c248c5dc8762fec41
Matthias Schaefer <matthias.schaefer@itk-engineering.de>
2025-12-16 07:16:05 +0100

    add ctrl-click for links to wezterm config

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -7,6 +7,16 @@ config.color_scheme = "Catppuccin Mocha"
 config.window_background_opacity = 0.7
 config.enable_tab_bar = false
 
+--- Link Behavior
+config.mouse_bindings = {
+	-- Ctrl-click will open the link under the mouse cursor
+	{
+		event = { Up = { streak = 1, button = "Left" } },
+		mods = "CTRL",
+		action = wezterm.action.OpenLinkAtMouseCursor,
+	},
+}
+
 --- Startup Behavior
 wezterm.on("gui-startup", function(cmd)
 	local tab, pane, window = mux.spawn_window(cmd or {})
